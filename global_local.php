bde219036b44b3a9f1c49d5981859193d25b8b3b
Kauã Nunes <kauanunes.dev09@gmail.com>
2026-04-08 12:20:25 -0300

    Add global_local

--- global_local.php
+++ global_local.php
@@ -9,4 +9,21 @@
     passandoParametros($nomePublico);
 
     echo "<br><br>";
+    // Função que não recebe parâmetro
+    function semPassarParametro1()
+    {
+        $mensagem = "boa noite";
+        echo $mensagem . $nomePublico;
+    }
+    semPassarParametro1();
+    echo "<br><br>";
+    function semParametro2()
+    {
+        $mensagem = "Boa noite!";
+        echo "Sem parâmetro 2" . $mensagem . $GLOBALS['nomePublico'];
+    }  
+    semParametro2();
+    // No php você só consegue acessar a variável (ou variáveis) globais através de "$GLOBALS" com o nome dentro dos colchetes
+    echo "<br><br>";
+    echo "<a href='index.php'>Voltar</a>"
 ?>
