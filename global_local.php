a8111e29f58e30aa8e76b0ef7b1191e1682b129b
Kauã Nunes <kauanunes.dev09@gmail.com>
2026-04-08 12:21:32 -0300

    transformar função para não ficar com erro

--- global_local.php
+++ global_local.php
@@ -10,12 +10,13 @@
 
     echo "<br><br>";
     // Função que não recebe parâmetro
-    function semPassarParametro1()
-    {
-        $mensagem = "boa noite";
-        echo $mensagem . $nomePublico;
-    }
-    semPassarParametro1();
+    // function semPassarParametro1()
+    // {
+    //     $mensagem = "boa noite";
+    //     echo $mensagem . $nomePublico;
+    // }
+    // semPassarParametro1();
+    // comentário para não ficar com erro
     echo "<br><br>";
     function semParametro2()
     {
